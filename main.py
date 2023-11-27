8a4b2ba4bf383d19c9f707c6e55a34aabd625d71
Luis <py.luis.hf.smtp@gmail.com>
2023-11-27 14:40:34 +0100

    added contact at end

--- main.py
+++ main.py
@@ -12,30 +12,30 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/contact", methods=["GET", "POST"])
-def contact():
-    if request.method == "POST":
-        data = request.form
-        send_email(data["name"], data["email"], data["message"])
-        return render_template("contact.html", msg_sent=True)
-    return render_template("contact.html", msg_sent=False)
+# @app.route("/contact", methods=["GET", "POST"])
+# def contact():
+#     # if request.method == "POST":
+#     #     data = request.form
+#     #     send_email(data["name"], data["email"], data["message"])
+#     #     return render_template("contact.html", msg_sent=True)
+#     # return render_template("contact.html", msg_sent=False)
+#     return render_template('contact_me.html')
+
+# MAIL_ADDRESS = os.environ.get('EMAIL')
+# MAIL_APP_PW = os.environ.get('PWD')
 
 
-MAIL_ADDRESS = os.environ.get('EMAIL')
-MAIL_APP_PW = os.environ.get('PWD')
-
-
-def send_email(name, email, message):
-    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
-    with smtplib.SMTP("smtp.gmail.com") as connection:
-        connection.starttls()
-        connection.login(MAIL_ADDRESS, MAIL_APP_PW)
-        connection.sendmail(
-            from_addr=MAIL_ADDRESS,
-            to_addrs=MAIL_ADDRESS,
-            msg=email_message
-        )
+# def send_email(name, email, message):
+#     email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nMessage:{message}"
+#     with smtplib.SMTP("smtp.gmail.com") as connection:
+#         connection.starttls()
+#         connection.login(MAIL_ADDRESS, MAIL_APP_PW)
+#         connection.sendmail(
+#             from_addr=MAIL_ADDRESS,
+#             to_addrs=MAIL_ADDRESS,
+#             msg=email_message
+#         )
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
